6dc52c165cb86fa3245047bcf01ca09cc72f262c
Matthew Brisebois <matthew@webheroes.ca>
2018-06-04 19:18:10 +0000

    Moved chain scope so it works even when Promise.chain is already defined

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 
-if (Promise && Promise.chain === undefined) {
-    Object.defineProperty(Promise, 'chain', {
-	value: function (chain, all) {
-	    if (all === undefined)
-		all	= [];
-	    var p	= chain.shift()
-	    if (typeof p === 'function')
-		p	= p.apply(all,all);
-	    if (p === null || typeof p !== 'object' || typeof p.then !== 'function')
-		return Promise.reject(Error("Chain method did not return a Promise object"));
-	    return p.then(function(data) {
-		all.push(data);
-		if (chain.length)
-		    return Promise.chain(chain, all);
-		else
-		    return Promise.resolve(all);
-	    }, function(err) {
-		return Promise.reject(err);
-	    });
-	}
+const chain = function (sequence, all) {
+    if (all === undefined)
+	all				= [];
+    
+    var p				= sequence.shift()
+    
+    if ( typeof p === 'function' && typeof p.then !== 'function' )
+	p				= p.apply(all,all);
+    
+    if (p === null || typeof p !== 'object' || typeof p.then !== 'function')
+	return Promise.reject( Error("Array item did not return a Promise object: " + typeof p) );
+    
+    return p.then(function(data) {
+	all.push(data);
+	if (sequence.length)
+	    return chain(sequence, all);
+	else
+	    return Promise.resolve(all);
+    }, function(err) {
+	return Promise.reject(err);
     });
 }
 
-module.exports = Promise.chain;
+if (Promise && Promise.chain === undefined) {
+    Object.defineProperty(Promise, 'chain', {
+	value: chain
+    });
+}
+
+module.exports = chain;
